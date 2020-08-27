a58ce5c327fef501a3d7fd2f2d9b6011c8cd78a6
Andreas Gustafsson <andreas.gustafsson88@gmail.com>
2020-08-27 12:24:37 +0200

    ---1 commit--- show numbers divisible by 7 but not by 5

--- divisible_multible.py
+++ divisible_multible.py
@@ -1,5 +1,14 @@
 def divisible_multible(start, end):
+    result = ""
+    end = end + 1
+    for number in range(start, end):
+        if number % 7 == 0 and not number % 5 == 0:
+            result += str(number) + ","
+    return result
 
+
+def main():
+    print(divisible_multible(2000, 3200))
 
 
 if __name__ == "__main__":
