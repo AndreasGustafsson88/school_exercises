92f4aa1f0112525ca35824c10e7af1027f0f9728
Andreas Gustafsson <andreas.gustafsson88@gmail.com>
2020-08-28 13:48:34 +0200

    --1 commit --

---
+++ prime_numbers.py
@@ -0,0 +1,16 @@
+def prime(input):
+    if input <= 1:
+        return False
+    for x in range(2, input):
+        if input % x == 0:
+            return False
+    return True
+
+
+def main():
+    for x in range (100):
+        print(x)
+
+
+if __name__ == "__main__":
+    main()
