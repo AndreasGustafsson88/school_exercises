aba028c5470738157536a16dd22bb6c54ecd31d1
Andreas Gustafsson <andreas.gustafsson88@gmail.com>
2020-08-28 12:07:33 +0200

    --1 commit--

---
+++ longest_word.py
@@ -0,0 +1,14 @@
+def longest_word(input1):
+    longest = ""
+    for i in input1:
+        if len(i) > len(longest):
+            longest = i
+    return longest
+
+
+def main():
+    my_words = ["that", "take", "a", "list", "of", "words", "and", "returns", "the", "length", "longest"]
+    print(longest_word(my_words))
+
+if __name__ == "__main__":
+    main()
